893bdec6f508966560aff6b7e390204f0a00bba6
Instinct <m16a1@live.de>
2025-07-06 23:32:30 +0200

    data externalization

--- shipyard/src/main/java/de/instinct/shipyard/service/impl/ShipyardServiceImpl.java
+++ shipyard/src/main/java/de/instinct/shipyard/service/impl/ShipyardServiceImpl.java
@@ -1,20 +1,13 @@
 package de.instinct.shipyard.service.impl;
 
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
-import java.util.UUID;
 
 import org.springframework.stereotype.Service;
 
 import base.file.FileManager;
-import de.instinct.api.construction.dto.WeaponType;
 import de.instinct.api.core.service.impl.ObjectJSONMapper;
 import de.instinct.api.shipyard.dto.ShipBlueprint;
-import de.instinct.api.shipyard.dto.ShipDefense;
-import de.instinct.api.shipyard.dto.ShipType;
-import de.instinct.api.shipyard.dto.ShipWeapon;
 import de.instinct.api.shipyard.dto.ShipyardData;
 import de.instinct.api.shipyard.dto.ShipyardInitializationResponseCode;
 import de.instinct.api.shipyard.dto.StatChangeResponse;
@@ -34,130 +27,8 @@ public class ShipyardServiceImpl implements ShipyardService {
 	@Override
 	public ShipyardInitializationResponseCode init(String token) {
 		if (userShipyards.containsKey(token)) return ShipyardInitializationResponseCode.ALREADY_INITIALIZED;
-		List<ShipBlueprint> ownedShips = new ArrayList<>();
-		ShipDefense hawkDefense = ShipDefense.builder()
-				.shield(4)
-				.armor(5)
-				.shieldRegenerationSpeed(0.2f)
-				.build();
-		ShipWeapon hawkWeapon = ShipWeapon.builder()
-				.type(WeaponType.PROJECTILE)
-				.damage(3)
-				.range(80f)
-				.speed(100f)
-				.cooldown(500)
-				.build();
-		ownedShips.add(ShipBlueprint.builder()
-				.uuid(UUID.randomUUID().toString())
-				.type(ShipType.FIGHTER)
-				.model("hawk")
-				.movementSpeed(70f)
-				.cost(4)
-				.commandPointsCost(1)
-				.defense(hawkDefense)
-				.weapon(hawkWeapon)
-				.inUse(true)
-				.build());
-		
-		ShipDefense turtleDefense = ShipDefense.builder()
-				.shield(15)
-				.armor(25)
-				.shieldRegenerationSpeed(0.2f)
-				.build();
-		ShipWeapon turtleWeapon = ShipWeapon.builder()
-				.type(WeaponType.LASER)
-				.damage(3)
-				.range(40f)
-				.speed(100f)
-				.cooldown(2000)
-				.build();
-		ownedShips.add(ShipBlueprint.builder()
-				.uuid(UUID.randomUUID().toString())
-				.type(ShipType.FIGHTER)
-				.model("turtle")
-				.movementSpeed(40f)
-				.cost(6)
-				.commandPointsCost(1)
-				.defense(turtleDefense)
-				.weapon(turtleWeapon)
-				.build());
-		
-		ShipDefense sharkDefense = ShipDefense.builder()
-				.shield(4)
-				.armor(6)
-				.shieldRegenerationSpeed(0.2f)
-				.build();
-		ShipWeapon sharkWeapon = ShipWeapon.builder()
-				.type(WeaponType.MISSILE)
-				.damage(6)
-				.range(100f)
-				.speed(60f)
-				.cooldown(2000)
-				.build();
-		ownedShips.add(ShipBlueprint.builder()
-				.uuid(UUID.randomUUID().toString())
-				.type(ShipType.FIGHTER)
-				.model("shark")
-				.movementSpeed(40f)
-				.cost(5)
-				.commandPointsCost(1)
-				.defense(sharkDefense)
-				.weapon(sharkWeapon)
-				.build());
-		
-		ShipDefense eelDefense = ShipDefense.builder()
-				.shield(4)
-				.armor(4)
-				.shieldRegenerationSpeed(0.2f)
-				.build();
-		ShipWeapon eelWeapon = ShipWeapon.builder()
-				.type(WeaponType.BEAM)
-				.damage(5)
-				.range(110f)
-				.speed(250f)
-				.cooldown(3000)
-				.build();
-		ownedShips.add(ShipBlueprint.builder()
-				.uuid(UUID.randomUUID().toString())
-				.type(ShipType.FIGHTER)
-				.model("eel")
-				.movementSpeed(60f)
-				.cost(6)
-				.commandPointsCost(1)
-				.defense(eelDefense)
-				.weapon(eelWeapon)
-				.build());
-		
-		ShipDefense cheetahDefense = ShipDefense.builder()
-				.shield(5)
-				.armor(10)
-				.shieldRegenerationSpeed(0.5f)
-				.build();
-		ShipWeapon cheetahWeapon = ShipWeapon.builder()
-				.type(WeaponType.BEAM)
-				.damage(10)
-				.range(120f)
-				.speed(300f)
-				.cooldown(3000)
-				.build();
-		ownedShips.add(ShipBlueprint.builder()
-				.uuid(UUID.randomUUID().toString())
-				.type(ShipType.FIGHTER)
-				.model("cheetah")
-				.movementSpeed(140f)
-				.cost(2)
-				.commandPointsCost(1)
-				.defense(cheetahDefense)
-				.weapon(cheetahWeapon)
-				.inUse(false)
-				.build());
-		ShipyardData shipyardData = ShipyardData.builder()
-				.ownedShips(ownedShips)
-				.slots(1)
-				.activeShipSlots(1)
-				.build();
+		ShipyardData shipyardData = ObjectJSONMapper.mapJSON(FileManager.loadFile("init.data"), ShipyardData.class);
 		userShipyards.put(token, shipyardData);
-		FileManager.saveFile("init.data", ObjectJSONMapper.mapObject(shipyardData));
 		return ShipyardInitializationResponseCode.SUCCESS;
 	}
 
--- starmap/src/main/java/de/instinct/starmap/service/impl/StarmapServiceImpl.java
+++ starmap/src/main/java/de/instinct/starmap/service/impl/StarmapServiceImpl.java
@@ -1,18 +1,14 @@
 package de.instinct.starmap.service.impl;
 
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 import org.springframework.stereotype.Service;
 
-import de.instinct.api.meta.dto.Resource;
-import de.instinct.api.meta.dto.ResourceAmount;
-import de.instinct.api.starmap.dto.GalaxyData;
+import base.file.FileManager;
+import de.instinct.api.core.service.impl.ObjectJSONMapper;
 import de.instinct.api.starmap.dto.SectorData;
 import de.instinct.api.starmap.dto.StarmapInitializationResponseCode;
-import de.instinct.api.starmap.dto.StarsystemData;
 import de.instinct.starmap.service.StarmapService;
 
 @Service
@@ -27,61 +23,8 @@ public class StarmapServiceImpl implements StarmapService {
 	@Override
 	public StarmapInitializationResponseCode init(String token) {
 		if (starmaps.get(token) != null) return StarmapInitializationResponseCode.ALREADY_INITIALIZED;
-		starmaps.put(token, SectorData.builder()
-				.galaxies(loadInitialGalaxies())
-				.build());
+		starmaps.put(token, ObjectJSONMapper.mapJSON(FileManager.loadFile("init.data"), SectorData.class));
 		return StarmapInitializationResponseCode.SUCCESS;
-	}
-
-	private List<GalaxyData> loadInitialGalaxies() {
-		List<ResourceAmount> testResourceRewards = new ArrayList<>();
-		testResourceRewards.add(ResourceAmount.builder()
-				.type(Resource.CREDITS)
-				.amount(500)
-				.build());
-		testResourceRewards.add(ResourceAmount.builder()
-				.type(Resource.METAL)
-				.amount(500)
-				.build());
-		testResourceRewards.add(ResourceAmount.builder()
-				.type(Resource.CRYSTALS)
-				.amount(500)
-				.build());
-		
-		StarsystemData testStarSystem = StarsystemData.builder()
-				.id(0)
-				.name("Teststarsystem")
-				.mapPosX(-20)
-				.mapPosY(-20)
-				.planets(6)
-				.ancientPoints(50)
-				.threatLevel(1)
-				.conquered(false)
-				.resourceRewards(testResourceRewards)
-				.build();
-		
-		GalaxyData testGalaxy = GalaxyData.builder()
-				.id(0)
-				.name("Testgalaxy")
-				.mapPosX(0)
-				.mapPosY(0)
-				.starsystems(new ArrayList<>())
-				.build();
-		testGalaxy.getStarsystems().add(testStarSystem);
-		
-		GalaxyData testGalaxy2 = GalaxyData.builder()
-				.id(1)
-				.name("Testgalaxy2")
-				.mapPosX(20)
-				.mapPosY(20)
-				.starsystems(new ArrayList<>())
-				.build();
-		testGalaxy2.getStarsystems().add(testStarSystem);
-
-		List<GalaxyData> galaxies = new ArrayList<>();
-		galaxies.add(testGalaxy);
-		galaxies.add(testGalaxy2);
-		return galaxies;
 	}
 
 	@Override
