2e9e870acbe410eda6c9e5f225b75e6a6544c5ad
Instinct <m16a1@live.de>
2026-01-29 09:15:47 +0100

    files moved for native builds

--- api/src/main/java/de/instinct/api/control/model/TableRow.java
+++ api/src/main/java/de/instinct/api/control/model/TableRow.java
@@ -3,13 +3,15 @@ package de.instinct.api.control.model;
 import java.util.List;
 
 import de.instinct.api.core.annotation.Dto;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
-import lombok.Data;
 import lombok.Builder.Default;
+import lombok.Data;
 
 @Dto
 @Data
 @Builder
+@AllArgsConstructor
 public class TableRow {
 	
 	private List<TableCell> cells;
